08a8f64a9ebd71d04903ed144841d8da39ca5af4
Grégory Quatannens <gregquat@gmail.com>
2013-07-23 14:20:27 +0200

    adding unit test

---
+++ Tests/GeneratorTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Camp\Bundle\Html2PdfBundle\Tests;
+
+use Camp\Bundle\Html2PdfBundle\DependencyInjection\PdfGenerator;
+
+class GeneratorTest extends \PHPUnit_Framework_TestCase
+{
+    public function testOutputPdf()
+    {
+        // Set something, else the tcpdf library cannot set the $kPathUrl variable
+        $_SERVER['HTTP_HOST'] = 'something';
+
+        $generator = new PdfGenerator();
+        ob_start();
+        $generator->outputPdf('<html>foo</html>',array('format'=>'L'));
+        $pdfData = ob_get_contents();
+        ob_end_clean();
+
+        $this->assertTrue(preg_match('/PDF/', $pdfData) == 1 && preg_match('/EOF/', $pdfData));
+    }
+
+}
