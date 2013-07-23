21a99ae98edbe75a2962ed6c69a60f0b60d18af5
Grégory Quatannens <gregquat@gmail.com>
2013-07-23 12:58:58 +0200

    adding features

--- DependencyInjection/PdfGenerator.php
+++ DependencyInjection/PdfGenerator.php
@@ -6,18 +6,62 @@ use Obtao\Bundle\Html2PdfBundle\lib\HTML2PDF;
 
 class PdfGenerator{
 	
-	public function generateHtml($content){
+	protected $font = 'Arial';
+	protected $format = 'P';
+	protected $language = 'en';
+	protected $size = 'A4';
+
+	public function getGeneratedPdf($content, array $options = array()){
+		$this->setDefaultOptions($options);
+
 		try
 	    {
-	    	$content = str_replace('<html>', '<page>', $content);
-	    	$content = str_replace('</html>', '</page>', $content);
-	        $html2pdf = new HTML2PDF('P', 'A4', 'fr');	
-	        $html2pdf->setDefaultFont('Arial');
-	        $html2pdf->writeHTML($content);
-	        $html2pdf->Output('');
+	    	$html2pdf = $this->createPdf($content);
+	        return $html2pdf;
 	    }
 	    catch(HTML2PDF_exception $e) {
 	        throw new \Exception($e->getMessage());
 	    }
 	}
+
+	public function generateFile($content, $filename, array $options = array()){
+		$this->setDefaultOptions($options);
+
+		try
+	    {	    	
+	        $html2pdf = $this->createPdf($content);
+	        $html2pdf->Output($filename);
+	    }
+	    catch(HTML2PDF_exception $e) {
+	        throw new \Exception($e->getMessage());
+	    }
+	}
+
+	public function outputPdf($content, array $options = array()){
+		$html2pdf = $this->getGeneratedPdf($content, $options);
+		$html2pdf->Output('');
+	}
+
+	protected function createPdf($content){
+		// replace the <html> tags by <page> tags
+    	$content = str_replace('<html>', '<page>', $content);
+    	$content = str_replace('</html>', '</page>', $content);
+
+		$html2pdf = new HTML2PDF($this->format, $this->size, $this->language);	
+        $html2pdf->setDefaultFont($this->font);
+        $html2pdf->writeHTML($content);
+
+        return $html2pdf;
+	}
+
+	protected function setDefaultOptions(array $options){
+		$properties = get_object_vars($this);
+
+		foreach($properties as $key=>$value){
+			if(isset($options[$key])){
+				$this->$key = $options[$key];
+			}
+		}
+	}
+
 }
